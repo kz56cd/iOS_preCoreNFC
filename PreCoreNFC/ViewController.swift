cc0314708447ee39582b1e512872bf6365b1432c
kz56cd <kz56cd@gmail.com>
2017-10-18 13:33:05 +0900

    Add coreNFC session and delegate, Add usage description in info.plist

--- PreCoreNFC/ViewController.swift
+++ PreCoreNFC/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
 
+    var session: NFCNDEFReaderSession!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        prepareNFCReaderSession()
+        startNFCReaderSession()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    private func prepareNFCReaderSession() {
+        session = NFCNDEFReaderSession(
+            delegate: self,
+            queue: nil,
+            invalidateAfterFirstRead: true
+        )
+    }
+    private func startNFCReaderSession() {
+        session.begin()
+    }
+}
 
-
+extension ViewController: NFCNDEFReaderSessionDelegate {
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for record in message.records {
+                if let type = String(data: record.type, encoding: .utf8) {
+                    print("type: \(type)")
+                }
+                if let identifier = String(data: record.identifier, encoding: .utf8) {
+                    print("identifier: \(identifier)")
+                }
+                if let payload = String(data: record.payload, encoding: .utf8) {
+                    print("type: \(payload)")
+                }
+            }
+        }
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(#function, error)
+    }
 }
 
